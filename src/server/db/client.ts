558eaf935de09dbf8b4bbe514186dec7288f6dfd
Shawn Cheng <43364792+chenghsj@users.noreply.github.com>
2026-04-27 01:41:21 +0800

    fix: ensure app directories exist before instantiating database

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,9 +1,9 @@
 import Database from 'better-sqlite3'
 import { drizzle } from 'drizzle-orm/better-sqlite3'
-import { getAppPaths } from '../app-paths'
+import { ensureAppDirectories } from '../app-paths'
 import * as schema from './schema'
 
-const paths = getAppPaths()
+const paths = ensureAppDirectories()
 const sqlite = new Database(paths.databaseFile)
 
 export const db = drizzle(sqlite, { schema })
